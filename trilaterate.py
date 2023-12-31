8c4fcba3356edab6d862518aa8cc8f7493fc65a8
aaix <55102797+aaix@users.noreply.github.com>
2023-12-31 05:57:24 +0000

    add translation layer for earth coordinates

--- trilaterate.py
+++ trilaterate.py
@@ -1,3 +1,10 @@
+import utm
+
+
+def metres_to_degrees(metres):
+    # 1 degree = 111 km
+    return metres / 111000
+
 
 def trilaterate(p1, p2, p3, r1, r2, r3):
     """2D coordinate trilateration"""
@@ -22,3 +29,19 @@ def trilaterate(p1, p2, p3, r1, r2, r3):
     y = (C * D - A * F) / (B * D - A * E)
 
     return (x,y)
+
+
+def earth_trilaterate(p1, p2, p3, r1, r2, r3, utm_zone=30, utm_letter='U'):
+    # cause the earth is round we need to map it to flat
+    # coordinates using the UTM projection
+
+    x1, y1, _, _ = utm.from_latlon(*p1)
+    x2, y2, _, _ = utm.from_latlon(*p2)
+    x3, y3, _, _ = utm.from_latlon(*p3)
+
+    # convert metres to degrees
+    r1 = metres_to_degrees(r1)
+    r2 = metres_to_degrees(r2)
+    r3 = metres_to_degrees(r3)
+
+    return utm.to_latlon(*trilaterate((x1, y1), (x2, y2), (x3, y3), r1, r2, r3), utm_zone, utm_letter)
